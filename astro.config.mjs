44ec9b806cf46fccd2074429436cd3b31bc455ea
Saif Abdelrazek <saif@saifabdelrazek.com>
2025-07-18 10:11:05 +0300

    Updated manifest details, streamline icon definitions, and enabled trailings handler experimental feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -52,10 +52,15 @@ export default defineConfig({
       fields: ["title", "description", "content"],
     }),
     AstroPWA({
+      base: "/",
+      scope: "/",
+      includeAssets: ["favicon.svg"],
+      registerType: "autoUpdate",
       manifest: {
         name: "Saif Abdelrazek Personal Website",
         short_name: "SaifSite",
-        description: "Portfolio, blog, and projects by Saif Abdelrazek.",
+        description:
+          "Personal website of Saif Abdelrazek || Not just a website",
         start_url: "/",
         scope: "/",
         display: "standalone",
@@ -65,60 +70,37 @@ export default defineConfig({
         lang: "en",
         icons: [
           {
-            src: "/favicon-192x192.png",
-            sizes: "192x192",
+            src: "/favicon-96x96.png",
+            sizes: "96x96",
             type: "image/png",
-            purpose: "maskable any",
           },
           {
-            src: "/favicon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable any",
+            src: "/favicon.svg",
+            sizes: "any",
+            type: "image/svg+xml",
           },
           {
             src: "/favicon.ico",
             sizes: "48x48 32x32 16x16",
             type: "image/x-icon",
           },
-        ],
-        shortcuts: [
           {
-            name: "Blog",
-            short_name: "Blog",
-            description: "Read my latest posts",
-            url: "/blog",
-            icons: [
-              { src: "/icons/blog.png", sizes: "96x96", type: "image/png" },
-            ],
-          },
-          {
-            name: "Projects",
-            short_name: "Projects",
-            description: "View my projects",
-            url: "/projects",
-            icons: [
-              { src: "/icons/projects.png", sizes: "96x96", type: "image/png" },
-            ],
-          },
-          {
-            name: "Portfolio",
-            short_name: "Portfolio",
-            description: "See my portfolio",
-            url: "/portfolio",
-            icons: [
-              {
-                src: "/icons/portfolio.png",
-                sizes: "96x96",
-                type: "image/png",
-              },
-            ],
+            src: "/apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+            purpose: "any",
           },
         ],
       },
-
+      workbox: {
+        navigateFallback: "/404",
+        globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
+      },
       devOptions: {
         enabled: true,
+      },
+      experimental: {
+        directoryAndTrailingSlashHandler: true,
       },
     }),
   ],
